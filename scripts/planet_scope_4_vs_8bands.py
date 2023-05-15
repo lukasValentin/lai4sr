0e4d73e3d2d073b5fae55e9551e4a2114f2bd843
lukas <graflu@ethz.ch>
2023-05-15 10:13:36 +0200

    plotting difference map

--- scripts/planet_scope_4_vs_8bands.py
+++ scripts/planet_scope_4_vs_8bands.py
@@ -78,6 +78,57 @@ def lai_scatter(
     plt.close(fig)
 
 
+def lai_difference_maps(
+        fpath_4_bands: Path,
+        fpath_8_bands: Path,
+        output_dir: Path) -> None:
+    """
+    Create difference maps of LAI values from Planet-Scope
+    with 4 and 8 bands.
+
+    Parameters
+    ----------
+    fpath_4_bands : Path
+        Path to the LAI values from Planet-Scope with 4 bands
+    fpath_8_bands : Path
+        Path to the LAI values from Planet-Scope with 8 bands
+    output_dir : Path
+        Path to the output directory
+    """
+
+    # read the LAI values
+    lai_4bands = RasterCollection.from_multi_band_raster(
+        fpath_4_bands)['lai']
+    lai_8bands = RasterCollection.from_multi_band_raster(
+        fpath_8_bands)['lai']
+
+    # calculate the difference (8 minus 4 band LAI)
+    lai_diff = lai_4bands - lai_8bands
+
+    # plot the differences with a diverging color map
+    fig, ax = plt.subplots(figsize=(5, 5))
+    lai_diff.plot(
+        colormap='coolwarm',
+        colorbar_label=r'Difference in LAI [$m^2$ $m^{-2}$]',
+        ax=ax
+    )
+    ax.set_title('PlanetScope 4-band LAI minus 8-band LAI')
+
+    # save the figure
+    scene = fpath_4_bands.stem.split('_')[0]
+    fname = output_dir / f'{scene}_lai_4_minus_8_bands.png'
+    fig.savefig(fname, dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    # histogram of differences
+    fig, ax = plt.subplots(figsize=(5,5))
+    lai_diff.hist(bins=100, ax=ax)
+    ax.set_xlabel(r'Difference in LAI [$m^2$ $m^{-2}$]')
+    fname = output_dir / f'{scene}_lai_4_minus_8_bands_hist.png'
+    fig.savefig(fname, dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+
 if __name__ == '__main__':
 
     fpath_4bands = Path(
@@ -87,5 +138,7 @@ if __name__ == '__main__':
 
     output_dir = Path('analysis/planetscope_4_vs_8_bands')
     output_dir.mkdir(exist_ok=True, parents=True)
+
+    lai_difference_maps(fpath_4bands, fpath_8bands, output_dir)
 
     lai_scatter(fpath_4bands, fpath_8bands, output_dir)
